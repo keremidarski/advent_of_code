024f6e870b3ed3b00b02886348277477e319945a
Ivaylo Keremidarski <ivaylo.keremidarski@gmail.com>
2022-12-06 16:26:13 +0200

    Create aoc_2022_day01_01.py

---
+++ 2022/aoc_2022_day01_01.py
@@ -0,0 +1,34 @@
+# AOC 2022 Day 01 Part 01
+
+def prep_input():
+    dwarfs = []
+
+    with open("./day01.in") as file:
+        data = file.read().split("\n\n")
+
+    for dwarf in data:
+        dwarfs.append([int(cal) for cal in dwarf.split("\n")])
+
+    return dwarfs
+
+
+def most_calories(dwarfs):
+    max_cal = 0
+
+    for dwarf in dwarfs:
+        if sum(dwarf) > max_cal:
+            max_cal = sum(dwarf)
+
+    return max_cal
+
+
+def main():
+    data = prep_input()
+    result = most_calories(data)
+    print(result)
+
+  
+  
+
+if __name__ == "__main__":
+    main()
