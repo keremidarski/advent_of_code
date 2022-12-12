55d6306a8279ce71ee98376053eda4cb03f776cb
Ivaylo Keremidarski <ivaylo.keremidarski@gmail.com>
2022-12-12 14:57:04 +0200

    Create aoc_2022_day12_02.py

---
+++ 2022/day12/aoc_2022_day12_02.py
@@ -0,0 +1,70 @@
+# AOC 2022 Day 12 Part 02
+
+
+from string import ascii_lowercase
+from heapq import heappop, heappush
+
+
+def prep_input():
+    grid = []
+
+    with open("./day12.in") as file:
+        for line in file.readlines():
+            grid.append(list(line.strip()))
+
+    return grid
+
+
+grid = prep_input()
+rows = len(grid)
+columns = len(grid[0])
+
+
+for y in range(rows):
+    for x in range(columns):
+        char = grid[y][x]
+
+        if char == "S":
+            start = y, x
+        if char == "E":
+            target = y, x
+
+
+def get_height(letter):
+    if letter in ascii_lowercase:
+        return ascii_lowercase.index(letter)
+    if letter == "S":
+        return 0
+    if letter == "E":
+        return 25
+
+
+def get_neighbours(y, x):
+    for dy, dx in [[1, 0], [-1, 0], [0, 1], [0, -1]]:
+        yy = y + dy
+        xx = x + dx
+
+        if not (0 <= yy < rows and 0 <= xx < columns):
+            continue
+
+        if get_height(grid[yy][xx]) >= get_height(grid[y][x]) - 1:
+            yield yy, xx
+
+
+visited = [[False] * columns for i in range(rows)]
+heap = [(0, target[0], target[1])]
+
+while True:
+    steps, y, x = heappop(heap)
+
+    if visited[y][x]:
+        continue
+
+    visited[y][x] = True
+
+    if get_height(grid[y][x]) == 0:
+        print(steps)
+        break
+
+    for yy, xx in get_neighbours(y, x):
+        heappush(heap, (steps + 1, yy, xx))
